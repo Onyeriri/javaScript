fb8ba4244fac6c1eb23322c56e777355349b7212
Onyeriri Victor <viktorchinonso@gmail.com>
2022-10-06 08:17:56 +0100

    Added regex feature to index.js

--- index.js
+++ index.js
@@ -1533,3 +1533,185 @@ const carrot = new Vegetable("carrot");
 console.log(carrot.name); // Should display 'carrot'
 
 // Use getters and setters to Control Access to an Object
+class Thermostat {
+  constructor(fahrenheit) {
+    this._fahrenheit = fahrenheit;
+  }
+
+  get temperature() {
+    return (5 / 9) * (this._fahrenheit - 32);
+  }
+
+  set temperature(celsius) {
+    this._fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
+// Create a Module Script
+<html>
+  <body>
+    {/* <!-- Only change code below this line --> */}
+    <script type="module" src="index.js"></script>
+    {/* <!-- Only change code above this line --> */}
+  </body>
+</html>;
+
+// Use export to Share a Code Block
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+};
+
+const lowercaseString = (string) => {
+  return string.toLowerCase();
+};
+
+export { uppercaseString, lowercaseString };
+
+// Reuse JavaScript Code Using import
+import { uppercaseString, lowercaseString } from "./string_functions.js";
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+// Use * to Import Everything from a File
+import * as stringFunctions from "./string_functions.js";
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+// Create an Export Fallback with export default
+export default function subtract(x, y) {
+  return x - y;
+}
+
+// Import a Default Export
+import subtract from "./math_functions.js";
+// Only change code above this line
+
+subtract(7, 4);
+
+// Create a JavaScript Promise
+const makeServerRequest = new Promise((resolve, reject) => {});
+
+// Complete a Promise with resolve and reject
+const keServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer = true;
+
+  if (responseFromServer) {
+    // Change this line
+    resolve("We got the data");
+  } else {
+    // Change this line
+    reject("Data not received");
+  }
+});
+
+// Handle a Fulfilled Promise with then
+const makeServerequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if (responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then((result) => {
+  console.log(result);
+});
+
+// Handle a Rejected Promise with catch
+const makerverRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+
+  if (responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then((result) => {
+  console.log(result);
+});
+
+makeServerRequest.catch((error) => {
+  console.log(error);
+});
+
+// Using the Test Method
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let resut = myRegex.test(myString); // Change this line
+
+// Match Literal Strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let reslt = waldoRegex.test(waldoIsHiding);
+
+// Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let rest = petRegex.test(petString);
+
+// Ignore Case While Matching
+let yString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let esult = fccRegex.test(myString);
+
+// Extract Matches
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let resl = extractStr.match(codingRegex); // Change this line
+
+// Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let sult = twinkleStar.match(starRegex); // Change this line
+
+// Match Anything with Wildcard Period
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let rult = unRegex.test(exampleStr);
+
+// Match Single Character with Multiple Possibilities
+let quoteSample =
+  "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let ret = quoteSample.match(vowelRegex); // Change this line
+console.log(result.length);
+
+// Match Letters of the Alphabet
+let qteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let lt = quoteSample.match(alphabetRegex); // Change this line
+
+// Match Numbers and Letters of the Alphabet
+let sample = "Blueberry 3.141592653s are delicious.";
+let gex = /[2-6h-s]/gi; // Change this line
+let relt = quoteSample.match(myRegex); // Change this line
+
+// Match Single Characters Not Specified
+let quSample = "3 blind mice.";
+let myx = /[^aeiou0-9]/gi; // Change this line
+let rlt = quoteSample.match(myRegex); // Change this line
+
+// Match Characters that Occur One or More Times
+let difficultSpelling = "Mississippi";
+let myReex = /s+/g; // Change this line
+let rt = difficultSpelling.match(myRegex);
+
+// Match Characters that Occur Zero or More Times
